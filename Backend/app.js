9f2ea4f2d700b050e510f785d9708a681fc2d04e
Kunal Vijay <kunalvijay.kv2002@gmail.com>
2023-06-06 18:12:17 +0530

    None

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //allows to access req.
 
 app.use(express.static("public"));
 
